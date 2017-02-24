008b76f9c4f1e8e6a345119bb98c8d2fc9b22cd9
wangyahui <wangyahui@mxtrip.cn>
2017-02-24 10:14:54 +0800

    update

---
+++ mysqldb/insertdb.py
@@ -0,0 +1,36 @@
+#encoding=utf-8
+import MySQLdb
+import time
+
+#db = MySQLdb.connect("localhost","testuser","test123","TESTDB" )
+db = MySQLdb.connect(host="120.26.49.233",port=3302, user="maindata",passwd="maindata123456!@#$%^",db="travel_main")
+f = open("zomato_site_map2","r")
+lines = f.readlines()
+
+cursor = db.cursor()
+
+for line in lines:
+       print line
+       strs = line.split("\t")
+       country = strs[0]
+       city = strs[1]
+       url = strs[2]
+       print country, city, url
+ 
+       time_str = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+       sql = """INSERT INTO crawl_info(src,
+             crawl_index1, crawl_index2, crawl_index3, create_time, update_time)
+             VALUES ('zomato', '"""+country+"""', '"""+city+"""', '"""+url+"""', '"""+time_str+"""','"""+time_str+"""')"""
+
+       try:
+              # 执行sql语句
+              cursor.execute(sql)
+              # 提交到数据库执行
+              db.commit()
+       except:
+              # Rollback in case there is any error
+              db.rollback()
+
+
+db.close()
